e1ae13fa5a3f42d22200c9cc8df29545ae0beb66
yl000github <450887644@qq.com>
2016-06-22 16:07:43 +0800

    6-22

--- app/scripts/randNum/randNum.js
+++ app/scripts/randNum/randNum.js
@@ -8,8 +8,9 @@ $(function(){
 		//20位
 		var bits=20;
 		var r="";
-		for(var i=0;i<bits;i++){
+		for(var i=1;i<=bits;i++){
 			r+=parseInt(Math.random()*10);
+			if(i%2==0) r+=" ";
 		}
 		$("#longNumResult").html(r);
 	});
@@ -18,5 +19,18 @@ $(function(){
 		var num=$("#randNumResult").html();
 		filepath=filepath.replace("1",num);
 		$("#imgResult").attr("src",filepath);
+	});
+	$("#show-tip").click(function(){
+		var r=$("#longNumResult").html();
+		var numArr=r.split(" ");
+		var html="";
+		for(var i=0;i<numArr.length;i++){
+			var n=numArr[i];
+//			console.log(n);
+			if(n.trim()=="") continue;
+			if(n.indexOf("0")==0) n=n.substring(1,2);
+			html+="<img class=\"pic\" src=\"../res/images/"+n+".png\" />";
+		}
+		$("#tip").html(html);
 	});
 })
